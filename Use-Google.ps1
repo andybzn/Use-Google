34eac4a79af793dfbaf4f6f45fe8dd2dc3ade25c
dark-coffee <9117837+dark-coffee@users.noreply.github.com>
2020-02-09 20:16:46 +0000

    Update Use-Google.ps1

--- Use-Google.ps1
+++ Use-Google.ps1
@@ -1,14 +1,41 @@
-#Script Name: Use-Google
-#Creator: Dark-Coffee
-#Version: 0.0.2
-#Updated: 29th September 2019
-#Url: https://github.com/dark-coffee/Use-Google
+<#
+Script Name: Use-Google.ps1
+Author: Dark-Coffee
+Version: 1.0.0
+Updated: 2020-02-09
+Url: https://github.com/dark-coffee/Use-Google/
+#>
+
+<#
+.Description
+    Take clipboard text and make it a LMGTFY link.
+.Parameter Browser
+    Switch to open the link in the default web browser
+.Example
+    Use-Google
+.Example
+    Use-Google -Browser
+#>
+
 
 function Use-Google {
-    $content = Get-Clipboard
-    $content1 = $content.Replace(' ','+')
-    $lmgtfy = "https://lmgtfy.com/?q="
-    $output = $lmgtfy + $content1
-    $output | clip
-    #& "C:\Program Files\internet explorer\iexplore.exe" "$output"
+    [Parameter(Mandatory=$false)][String]$Browser
+
+    #Get Clipboard Content
+    $ClipboardContent = (Get-Clipboard) -Replace(' ','+')
+
+    #Set URL
+    $LMGTFY = "https://lmgtfy.com/?q="
+
+    #Combine URL and Clipboard Content
+    $FunctionOutput = $LMGTFY + $ClipboardContent
+
+    if($Browser){
+        #Open Link in Browser
+        Start-Process -Path "$FunctionOutput"
+    }else{
+        #Copy Output to Clipboard
+        $FunctionOutput | Clip
+    }
+    
 }
